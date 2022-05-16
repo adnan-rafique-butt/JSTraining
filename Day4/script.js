00632c82c4d4c53e94fa3e4d62f98324dd3b0e1e
adnan-rafique-butt <adnan.rafique@invozone.com>
2022-05-16 17:20:43 +0500

    Training Day 4 Boolean and strings

--- Day4/script.js
+++ Day4/script.js
@@ -101,3 +101,87 @@ document.querySelector(
 document.querySelector(
   "#tovalue"
 ).innerHTML = `${num1} with valueOf Function, ${num1.valueOf()}`;
+
+/////Booleans
+//ToString
+const bool = false;
+document.querySelector("#toboolstring").innerHTML = bool.toString();
+//ToValue
+document.querySelector("#valueOfbool").innerHTML = bool.valueOf();
+
+////Strings
+//Length
+const str1 = "Text For String: ";
+const str = "Working From Office Increases Your Productivity.";
+document.querySelector("#strlength").innerHTML = str.length;
+document.querySelector(
+  "#strcharat"
+).innerHTML = `using charAt chracter at 5 index is "${str.charAt(5)}"`;
+document.querySelector(
+  "#strcharcode"
+).innerHTML = `using charCodeAt returns chracter  Unicode like  Unicode of index 5 is "${str.charCodeAt(
+  5
+)}"`;
+
+document.querySelector("#strconcat").innerHTML = str1.concat(str);
+
+document.querySelector(
+  "#strIndexOf"
+).innerHTML = `Index of Office in str is ${str.indexOf("Office")}`;
+
+document.querySelector(
+  "#strlastindex"
+).innerHTML = `LastIndex of Increases in str is ${str.lastIndexOf(
+  "Increases",
+  15
+)}`;
+
+///LocalCompare
+const elements = ["gfg", "geeksforgeeks", "cse", "department", "apple"];
+const compareSort = elements.sort((a, b) => a.localeCompare(b));
+document.querySelector("#strlocaleCompare").innerHTML = compareSort;
+
+document.querySelector("#strmatch").innerHTML = str.match(/ro/gi);
+
+//Replace
+document.querySelector("#strReplace").innerHTML = str.replace(
+  "Increases",
+  "Decreases"
+);
+
+//search
+document.querySelector("#strsearch").innerHTML = str.search("Your");
+
+//slice
+document.querySelector(
+  "#strslice"
+).innerHTML = `slicing string from 5 to 20 "${str.slice(5, 20)}"`;
+
+//split
+document.querySelector("#strsplit").innerHTML = str.split(" ", 4);
+
+//substr
+document.querySelector("#strsubstr").innerHTML = str.substr(1, 5);
+
+//substring
+document.querySelector("#strsubstring").innerHTML = str.substring(1, 5);
+
+///////Arraysss,
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+const ages = [32, 33, 17, 40];
+//Concat
+document.querySelector("#arrconcat").innerHTML = arr1.concat(arr2);
+
+function checkAge(age) {
+  return age >= 18;
+}
+document.querySelector(
+  "#arrevery"
+).innerHTML = `.every(func/expression) checks if every element of array is passing the condition or not and returns false if any of the element is not making condition true "${ages.every(
+  checkAge
+)}"`;
+const filteredAges = ages.filter(checkAge);
+document.querySelector(
+  "#arrfilter"
+).innerHTML = `Ouput of checking age in an array using filter ${filteredAges} and its type is ${typeof filteredAges}`;
